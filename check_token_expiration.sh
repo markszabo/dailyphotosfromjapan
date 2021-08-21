c84133276c9483af70e2fffcbb5780430147751a
markszabo <12611720+markszabo@users.noreply.github.com>
2021-08-21 16:33:17 +0900

    Fix bug

--- check_token_expiration.sh
+++ check_token_expiration.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 # how long before the expiration start failing
 ((timelimit=7*24*60*60)) # 1 week
 
-expiration_timestamp=$(curl "https://graph.facebook.com/v11.0/debug_token?access_token=$IG_ACCESS_TOKEN&debug=all&format=json&input_token=$IG_ACCESS_TOKEN" | jq -r -c ".data.expires_at")
+expiration_timestamp=$(curl --fail "https://graph.facebook.com/v11.0/debug_token?access_token=$IG_ACCESS_TOKEN&debug=all&format=json&input_token=$IG_ACCESS_TOKEN" | jq -r -c ".data.expires_at")
 current_timestamp=$(date +"%s")
 
 if [[ "$current_timestamp + $timelimit" -gt "$expiration_timestamp" ]]; then
--- upload_photo.sh
+++ upload_photo.sh
@@ -15,7 +15,7 @@ CAPTION="Follow for daily photos from all over Japan!
 #japan #japaneseculture #japanlife #japan_photo #japangram #instagramjapan #japandaily #instajapan #instadaily #japanpic #japanbestpics #beautifuljapan #japon #ig_japan #japantrip #igjapan #japanlover #travel #tokyo #anime #japanese #art #manga #travel #photography #kyoto #visitjapan #旅行 #日本 #国内旅行"
 PHOTOS_FOLDER=photos/
 USED_PHOTOS_FOLDER=usedphotos/
-PUBLIC_URL_PREFIX=https://github.com/markszabo/dailyphotosfromjapan/raw/main/$PHOTOS_FOLDER
+PUBLIC_URL_PREFIX=https://github.com/markszabo/dailyphotosfromjapan/raw/main/
 GIT_EMAIL=12611720+markszabo@users.noreply.github.com
 GIT_NAME=dailyphotosfromjapan
 
@@ -28,7 +28,7 @@ echo "Photo selected: $PHOTO with URL: $PHOTO_URL"
 # Upload photo
 echo "Uploading photo"
 CAPTION_URL_ENCODED=$(printf %s "$CAPTION" | jq -sRr @uri)
-UPLOAD_RESPONSE=$(curl -X POST "https://graph.facebook.com/v10.0/$IG_ACCOUNT_ID/media?image_url=$PHOTO_URL&caption=$CAPTION_URL_ENCODED&access_token=$IG_ACCESS_TOKEN")
+UPLOAD_RESPONSE=$(curl --fail -X POST "https://graph.facebook.com/v10.0/$IG_ACCOUNT_ID/media?image_url=$PHOTO_URL&caption=$CAPTION_URL_ENCODED&access_token=$IG_ACCESS_TOKEN")
 echo "Upload response:"
 echo "$UPLOAD_RESPONSE" # e.g. {"id":"17918147077712345"}
 UPLOAD_ID=$(echo "$UPLOAD_RESPONSE" | jq -r -c ".id")
@@ -36,7 +36,7 @@ echo "Parsed upload ID: $UPLOAD_ID"
 
 # Publish photo
 echo "Publishing photo"
-PUBLISH_RESPONSE=$(curl -X POST "https://graph.facebook.com/v10.0/$IG_ACCOUNT_ID/media_publish?creation_id=$UPLOAD_ID&access_token=$IG_ACCESS_TOKEN")
+PUBLISH_RESPONSE=$(curl --fail -X POST "https://graph.facebook.com/v10.0/$IG_ACCOUNT_ID/media_publish?creation_id=$UPLOAD_ID&access_token=$IG_ACCESS_TOKEN")
 echo "Publish response:"
 echo "$PUBLISH_RESPONSE" # e.g. {"id":"17973968953123456"}
 
