ae20abfe9b86671f1040a120a1c743406fe0d67d
markszabo <12611720+markszabo@users.noreply.github.com>
2021-08-21 22:47:52 +0900

    Improve logging

--- check_token_expiration.sh
+++ check_token_expiration.sh
@@ -7,7 +7,12 @@ set -euo pipefail
 expiration_timestamp=$(curl --fail "https://graph.facebook.com/v11.0/debug_token?access_token=$IG_ACCESS_TOKEN&debug=all&format=json&input_token=$IG_ACCESS_TOKEN" | jq -r -c ".data.expires_at")
 current_timestamp=$(date +"%s")
 
+((remaining=(expiration_timestamp-current_timestamp)/60/60/24))
+echo "The token will expire after $remaining days"
+
 if [[ "$current_timestamp + $timelimit" -gt "$expiration_timestamp" ]]; then
-  echo "Token is getting close to expiration, please replace it and put the new one to the github secret."
+  echo "Token is getting close to expiration, please replace it and put the new one into the Github secret."
   exit 1
+else
+  echo "Token is still valid for a while, no action required"
 fi
